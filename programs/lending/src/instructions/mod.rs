6180167b7e43cfe50e5402c63f89a7e34690ce4b
José Santos <josesantos@MBP-de-Jose-2.lan>
2025-01-10 15:12:39 +0000

    Borrow instructions

--- programs/lending/src/instructions/mod.rs
+++ programs/lending/src/instructions/mod.rs
@@ -1,6 +1,9 @@
 pub mod admin;
 pub mod deposit;
 pub mod withdraw;
+pub mod borrow;
+
 pub use admin::*;
 pub use deposit::*;
 pub use withdraw::*;
+pub use borrow::*;
--- programs/lending/src/state/bank.rs
+++ programs/lending/src/state/bank.rs
@@ -7,10 +7,13 @@ pub struct Bank {
   pub mint_address: Pubkey,
   pub total_deposits: u64,
   pub total_deposit_shares: u64,
+  pub total_borrowed: u64,
+  pub total_borrowed_shares: u64,
   //asset specific constants:
   pub liquidation_threshold: u64, // the loan to value (LTV) at which the loan is defined undercollateralized and that can be liquidated
   pub liquidation_bonus: u64, //% of liquidation that is given to the liquidator
   pub liquidation_close_factor: u64, //% of collateral that can be liquidated
   pub max_ltv: u64, //max % collateral that can be borrowed for a specific asset
   pub last_update: i64,
+  pub interest_rate: u64,
 }
